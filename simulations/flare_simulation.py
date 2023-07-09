6401547fea66095043f934c9d99204409cc60fb0
ori <ori@efrontier.io>
2023-07-09 14:34:15 +0300

    flare random

--- simulations/flare_simulation.py
+++ simulations/flare_simulation.py
@@ -813,13 +813,12 @@ class flare_simulation():
 
 if __name__ == '__main__':
     save_all_the_way = False
-
-    # save_time_series =  False
-    # save_images = False
-    # initail_seed = int(sys.argv[1])
-    # collateral_asset_name = sys.argv[2]
-    # total_runs = 50
-    # Parallel(n_jobs=10)(delayed(flare_simulation().run_random_simulation)(collateral_asset_name, initail_seed + j) for j in range(total_runs))
+    save_time_series =  False
+    save_images = False
+    initail_seed = int(sys.argv[1])
+    collateral_asset_name = sys.argv[2]
+    total_runs = 50
+    Parallel(n_jobs=10)(delayed(flare_simulation().run_random_simulation)(collateral_asset_name, initail_seed + j) for j in range(total_runs))
 
     # collateral_asset_name = sys.argv[1]
     # flare_simulation().analyaze_random_results(collateral_asset_name)
@@ -832,7 +831,7 @@ if __name__ == '__main__':
     # flare_simulation().find_ef_on_results("Doge","webserver\\flare\\REGULAR_DOGE\\summary.csv", False)
     # flare_simulation().find_ef_on_results("Xrp","webserver\\flare\\REGULAR_XRP\\summary.csv", False)
 
-    collateral_asset_name = sys.argv[1]
-    save_time_series = False
-    save_images = True
-    flare_simulation().run_regular_simulation(collateral_asset_name)
+    # collateral_asset_name = sys.argv[1]
+    # save_time_series = False
+    # save_images = True
+    # flare_simulation().run_regular_simulation(collateral_asset_name)
